5ad6037f62b7645eb98b887811673b1c8f14ac4f
MadihaAftab <madeeha.hiba123@gmail.com>
2022-01-02 00:20:04 +0500

    <Your message>

---
+++ assignment # 1/assignment.py
@@ -0,0 +1,39 @@
+#----------------------------------------------------------------------QUESTION NO 01---------------------------------------------------------------------------------------------------------------
+
+print ("Twinkle, twinkle, little star,\n \tHow I wonder what you are!")
+print ("\t \tUp above the world so high, \n \t \tLike a diamond in the sky.")
+print ("Twinkle, twinkle, little star,\n \tHow I wonder what you are!")
+
+#----------------------------------------------------------------------QUESTION NO 02---------------------------------------------------------------------------------------------------------------
+       
+import sys
+print("Python version")
+print (sys.version)
+
+#----------------------------------------------------------------------QUESTION NO 03---------------------------------------------------------------------------------------------------------------
+
+import datetime
+Current_Time = datetime.datetime.now()
+print ("Current date and time : ")
+print (Current_Time)
+
+#----------------------------------------------------------------------QUESTION NO 04---------------------------------------------------------------------------------------------------------------
+
+from math import pi
+radius = float (input ("Enter Radius of a circle :"))
+Area = pi*(radius)**2
+print (Area)
+
+#----------------------------------------------------------------------QUESTION NO 05---------------------------------------------------------------------------------------------------------------
+
+First_name = input ("Enter your First name:")
+Last_name = input ("Enter your last name:")
+print (Last_name , First_name)
+
+#----------------------------------------------------------------------QUESTION NO 06---------------------------------------------------------------------------------------------------------------
+
+a = int (input("Enter first number:"));
+b = int (input("Enter second number:"));
+c = a+b;
+print(c)
+
